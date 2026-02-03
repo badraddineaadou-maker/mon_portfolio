af1659852006aa50bc86519e566d67a8544a8667
Badr Eddine Aadou <badraddineaadou@gmail.com>
2026-02-03 15:27:51 +0100

    depaner le code php

--- send.php
+++ send.php
@@ -7,3 +7,10 @@ $sujet = $_POST['subject'];
 $message = $_POST['message'];
 $to = "badraddineaadou@gmail.com";
 $headers = "From: " . $email;
+
+// Dir l-condition hna bach tsift l-mail w t-verifiyi l-natija
+if (mail($to, $sujet, $message, $headers)) {
+    echo "L-mail tsift b-naja7!";
+} else {
+    echo "Kayn mouchkil, l-mail matsafetch.";
+}
